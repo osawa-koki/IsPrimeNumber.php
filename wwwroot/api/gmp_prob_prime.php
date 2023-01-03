0d46f1f94ee0d1a827bd2102f4b8a9791e34f328
osawa-koki <github@osawa.dev>
2023-01-04 03:20:03 +0900

    gmp_prob_primeの修正。

--- wwwroot/api/gmp_prob_prime.php
+++ wwwroot/api/gmp_prob_prime.php
@@ -1,10 +1,6 @@
 <?php
-require_once('../common/json.php');
 
-function gmp_prob_prime($n) {
-  $n = gmp_init($n);
-  return gmp_prob_prime($n);
-}
+require_once('../common/json.php');
 
 $requestData = get_json_from_stream();
 
@@ -12,6 +8,10 @@ if (isset($requestData->number)) {
   $n = $requestData->number;
   $result = gmp_prob_prime($n);
   $response = array('result' => $result);
+  echo json_encode($response);
+} else {
+  http_response_code(400);
+  $response = array('error' => 'No number provided');
   echo json_encode($response);
 }
 
