02aaa1ea5a718e2219efe5df24a57b4d6ad04403
osawa-koki <github@osawa.dev>
2023-01-04 02:28:23 +0900

    小さな修正。

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,7 +1,6 @@
 import React, { ReactNode } from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
-import setting from '../setting';
 import Pages from './pages';
 
 type Props = {
@@ -15,7 +14,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="shortcut icon" href={`${setting.basePath}favicon.ico`} type="image/x-icon" />
+      <link rel="shortcut icon" href={`/favicon.ico`} type="image/x-icon" />
     </Head>
     <header>
       <Pages />
